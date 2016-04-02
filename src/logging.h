1c6c0f067283e67969551a377f1fe98589045e0a
Gautam Gupta <blerb795@gmail.com>
2016-04-02 01:19:36 -0700

    mutex on streams

--- src/logging.h
+++ src/logging.h
@@ -2,18 +2,19 @@
 #define LOGGING_H
 
 #include <iostream>
-
-#ifndef _DEBUG
+#include <mutex>
+static std::mutex _mutex;
+#ifdef _DEBUG
+#define LOG_INFO _mutex.lock(); std::clog << __TIME__ << " [INFO] "
+#define LOG_END std::endl; _mutex.unlock();
+#define LOG_ERROR _mutex.lock(); std::cerr << __TIME__ << " [ERROR] "
+#else
 #include <fstream>
 static std::ofstream _nullstream; // use an unopened fstream as a "null" stream
-#define LOG_STREAM _nullstream
-#else
-#define LOG_STREAM std::clog      // log to std::clog if in debug mode
+#define LOG_INFO _nullstream
+#define LOG_END std::endl; _mutex.unlock();
+#define LOG_ERROR _mutex.lock(); std::cerr << __TIME__ << " [ERROR] "
 #endif
 
-#define LOG_INFO LOG_STREAM << __FILE__ << ':' << __LINE__ << " [INFO] "
-#define LOG_END std::endl
-
-#define LOG_ERROR std::cerr << __FILE__ << ':' << __LINE__ << " [ERROR] "
 
 #endif
